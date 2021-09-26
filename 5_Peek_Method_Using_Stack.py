f0640c1ea8493c20c2bf12d4040d0156d4edecde
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 11:26:01 +0530

    Update 5_Peek_Method_Using_Stack.py

--- 5_Peek_Method_Using_Stack.py
+++ 5_Peek_Method_Using_Stack.py
@@ -1,2 +1,32 @@
 class Stack:
+  def __init__(self):
+    self.lists = []
+  def __str__(self):
+    values = self.lists.reverse()
+    values = [str(x) for x in self.lists)
+    return "\n".join(values)
+              
+  def isEmpty(self):
+    if self.lists = []:
+       return True
+    else:
+       return False
+              
+  def push(self,values):
+              self.lists.append(values)
+              return "The element has been successfully been added"
+          
+   def pop(self):
+              if self.isEmpty():
+                return "The stack is empty"
+              else:
+                self.lists.pop()
+              
+   def peek(self):
+              if self.isEmpty():
+                return "Stack is Empty"
+              else:
+                self.lists[len(self.lists)-1]
+              
+            
   
