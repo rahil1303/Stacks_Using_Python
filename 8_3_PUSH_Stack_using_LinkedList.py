4f7205c4c374cf5519889c3afee9e1d04f8179a1
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 18:10:50 +0530

    Update 8_3_PUSH_Stack_using_LinkedList.py

--- 8_3_PUSH_Stack_using_LinkedList.py
+++ 8_3_PUSH_Stack_using_LinkedList.py
@@ -1,1 +1,23 @@
 class Node:
+  def __init__(self,value = None):
+    self.value = value
+    self.next = next
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+class Stack:
+  def __init__(self):
+    self.LinkedList = LinkedList()
+    
+   def isEmpty(self):
+    if self.LinkedList.head == None:
+      return True
+    else:
+      return False
+    
+    def push(self,value):
+      node = Node(value)
+      node.next = self.LinkedList.head
+      self.LinkedList.head = node
+ 
