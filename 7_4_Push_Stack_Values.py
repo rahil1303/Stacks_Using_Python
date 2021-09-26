aa44b7910f5ce77167276c3a4ee0e993fe84fc6a
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 13:02:50 +0530

    Update 7_4_Push_Stack_Values.py

--- 7_4_Push_Stack_Values.py
+++ 7_4_Push_Stack_Values.py
@@ -1,2 +1,31 @@
 class stack:
+  def __init__(self,maxsize):
+    self.maxsize = maxsize
+    self.lists = []
+    
+   def __str__(self):
+    values = self.list.reverse()
+    values = [str(x) for x in self.lists]
+    return "\n".join(values)
+  
+  def isFull(self):
+    if len(self.lists) == self.maxsize:
+      return True
+    else:
+      return False
+  
+  def isEmpty(self):
+    if self.lists == []:
+      return True
+    else:
+      return False
+    
+  def push(self,values):
+    if self.isFull():
+      return "The stack is Full"
+    else:
+      self.list.append(value)
+      return "The value was entered successfully"
+    
+  
   
