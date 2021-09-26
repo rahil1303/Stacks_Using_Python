a60874bdc3335c645516ba6b5d2aa1fa0e12169b
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 12:55:00 +0530

    Create 7_3_Empty_Stack.py

---
+++ 7_3_Empty_Stack.py
@@ -0,0 +1,14 @@
+class stack:
+  def __init__(self,maxsize):
+    self.maxsize = maxsize
+    self.list = []
+  def __str__(self):
+    values = self.lists.reverse()
+    values = [str(x) for x in self.lists]
+    return "\n".join(values)
+  
+  def isEmpty(self):
+    if self.list == []:
+      return True
+    else:
+      return False
