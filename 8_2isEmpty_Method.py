a8fbadb89350ee94b891efb87add6d081073f20b
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 18:03:57 +0530

    Update 8_2isEmpty_Method.py

--- 8_2isEmpty_Method.py
+++ 8_2isEmpty_Method.py
@@ -1,2 +1,21 @@
 class Node:
+  def __init__(self,value = None):
+    self.value = value
+    self.next = next
+    
+class LinkedList:
+  def _init__(self):
+    self.head = None
+    
+class Stack:
+  def __init__(self):
+    self.LinkedList = LinkedList()
+    
   
+  
+### isEmpty() function
+  def isEmpty(self):
+    if self.LinkedList.head == None:
+      return True
+    else:
+      return False
