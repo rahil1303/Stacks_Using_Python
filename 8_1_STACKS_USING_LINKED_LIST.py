0ea453c18a001739a1e1bfbb76253e076ffcfa5a
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 17:59:41 +0530

    Update 8_1_STACKS_USING_LINKED_LIST.py

--- 8_1_STACKS_USING_LINKED_LIST.py
+++ 8_1_STACKS_USING_LINKED_LIST.py
@@ -1,1 +1,12 @@
-class Node:
+class node:
+  def __init__(self,value = node):
+    self.value = value
+    self.next = next
+    
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    
+class Stack:
+  def __init__(self):
+    self.LinkedList = LinkedList()
