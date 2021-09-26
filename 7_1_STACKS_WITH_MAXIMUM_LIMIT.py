37e909678ce3ec1fc87448c3e53b52719b9d581a
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 12:49:27 +0530

    Update 7_1_STACKS_WITH_MAXIMUM_LIMIT.py

--- 7_1_STACKS_WITH_MAXIMUM_LIMIT.py
+++ 7_1_STACKS_WITH_MAXIMUM_LIMIT.py
@@ -1,1 +1,9 @@
 class stack:
+  def __init__(self,maxsize):
+    self.maxsize = maxsize
+    self.list = []
+  def __str__(self):
+    values = self.list.reverse()
+    values = [str(x) for x in self.list]
+    return "\n".join(values)
+  
