d417a1fc1d543e54bd90e42a43330dcc0779bc29
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 18:20:10 +0530

    Update 8_4_Pop_LinkedList.py

--- 8_4_Pop_LinkedList.py
+++ 8_4_Pop_LinkedList.py
@@ -1,2 +1,40 @@
 class Node:
-sel
+  def __init__(self,value = node):
+    self.value = value
+    self.next = next
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+class stack:
+  def __init__(self):
+    self.LinkedList = LinkedList()
+    
+  def isEmpty(self):
+    if self.LinkedList == None:
+      return True
+    else:
+      return False
+   
+  def push(self,value):
+    node = node(value)
+    node.next = self.LinkedList.head
+    self.LinkedList.head = node
+    
+  def pop(self):
+    if self.isEmpty():
+      return "there is no element in the stack"
+    else:
+      nodeValue = self.LinkedList.head.value
+      self.LinkedList.head = self.LinkedList.head.next
+      return nodeValue
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
