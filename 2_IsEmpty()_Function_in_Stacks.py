6823ba0261393219916d8fdb4a46095256392cab
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-26 10:43:00 +0530

    Create 2_IsEmpty()_Function_in_Stacks.py

---
+++ 2_IsEmpty()_Function_in_Stacks.py
@@ -0,0 +1,15 @@
+Class Stack():
+  def __init__(self):
+    self.lists = []
+  def __str__(self):
+    values = self.lists.reverse()
+    values = [str(x) for x in self.lists]
+    return "\n".join(values)
+    
+ 
+ ### IsEmpty() Function
+  def isEmpty(self):
+    if self.lists = []:
+      return True
+    else:
+      return False
