6e442670019fa2dd04ae98b5d18dc2a14882987c
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-10-05 23:24:31 +0530

    Create PROBELEM_4_STACK_OF_PLATES.py

---
+++ PROBELEM_4_STACK_OF_PLATES.py
@@ -0,0 +1,26 @@
+lass PlateStack():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.stacks = []
+    def __str__(self):
+        return self.stacks
+
+    def push(self,item):
+        if len(self.stacks) > 0 and (len(self.stacks[-1]) < self.capacity):
+            self.stacks[-1].append(item)
+        else:
+            self.stacks.append(item)
+    
+    def pop(self):
+        while len(self.stacks) and len(self.stacksstacks[-1] == 0):
+            self.stacks.pop()
+            if len(self.stacks) == 0:
+                return None
+            else:
+                return self.stacks[-1].pop()
+
+    def pop_at(self,stacknumber):
+        if len(self.stacks[stacknumber]) > 0:
+            return self.stacks[stacknumber].pop()
+        else:
+            return None
